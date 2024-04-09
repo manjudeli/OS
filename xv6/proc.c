64928e0cc21abcb5fc301e7223bfbf4e4245fa46
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-04-09 12:31:41 +0900

    Update proc.c

--- xv6/proc.c
+++ xv6/proc.c
@@ -248,6 +248,23 @@ exit(void)
     }
   }
 
+void exit2(int status)
+{
+  struct proc *curproc = myproc();
+  struct proc *p;
+  int fd;
+
+  if(curproc == initproc)
+    panic("init exiting");
+
+  // Close all open files.
+  for(fd = 0; fd < NOFILE; fd++){
+    if(curproc->ofile[fd]){
+      fileclose(curproc->ofile[fd]);
+      curproc->ofile[fd] = 0;
+    }
+  }
+
   begin_op();
   iput(curproc->cwd);
   end_op();
@@ -293,6 +310,55 @@ wait(void)
       if(p->state == ZOMBIE){
         // Found one.
         pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+int
+wait2(int *status)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        *status = p -> xstate;
+        pid = p->pid;
+        // 추가된 부
+        if(status != NULL){
+          if(copyout(curproc -> pgdir, (uint) status, &(p -> xstate), sizeof(int)) < 0){
+            release(&ptable.lock);
+            return -1;
+          }
+        }
         kfree(p->kstack);
         p->kstack = 0;
         freevm(p->pgdir);
